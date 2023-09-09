581273b874626828a200287100da32b86fdf9cf1
rafoshi <rafa.ioshi@gmail.com>
2023-09-09 02:30:59 -0300

    Lista de feedbacks finalizada

--- src/main/resources/static/scripts/feedbackList.js
+++ src/main/resources/static/scripts/feedbackList.js
@@ -8,19 +8,14 @@ function fillTable(feedbacks){
 
     let linhas = [];
     feedbacks.forEach(feedback => {
-        feedback.data = feedback.data.replace(/\u202F/g, ' ');
-        var date = new Date(feedback.data);
-        const dataFormatada = date.toLocaleDateString('pt-BR', {
-            year: 'numeric',
-            month: 'numeric',
-            day: 'numeric',
-        });
+        const dataFormatada = formataData(feedback.data);
 
         let linha = {
             "Data": dataFormatada,
             "Produto": feedback.fk_produto.nome,
             "ESG": feedback.tipo_esg,
             "Nota": feedback.nota,
+            "Id" : feedback.id,
         }
         linhas.push(linha);
     })
@@ -36,7 +31,14 @@ function fillTable(feedbacks){
         let nota = row.insertCell(3);
         nota.innerHTML = linha["Nota"];
         let detalhe = row.insertCell(4);
-        detalhe.innerHTML = "Ver detalhes";
+        let button = document.createElement("button");
+        button.type = "button";
+        button.className = "btn btn-primary";
+        button.setAttribute("data-toggle", "modal");
+        button.setAttribute("onclick", `detail(${linha.Id})`);
+        button.setAttribute("data-target", "#exampleModal");
+        button.textContent = "Ver detalhes";
+        detalhe.appendChild(button);
     })
 }
 
@@ -44,17 +46,63 @@ function fillTable(feedbacks){
 const selectElement = document.getElementById('esg');
 const produtoInput = document.getElementById('product-input');
 
+let esgSelected = 'Todos';
+const selectedDefault = document.querySelector('#esg')
+selectedDefault.value = 'Todos'
 selectElement.addEventListener("change", function() {
     const selectedValue = selectElement.value;
-    console.log("Opção selecionada: " + selectedValue);
-
+    esgSelected = selectedValue;
+    const searchText = document.querySelector('#product-input')
+    search(searchText.value);
 });
 
 produtoInput.addEventListener('input', function(event) {
     const textoDigitado = event.target.value;
+    search(textoDigitado);
+});
+
+function search(searchText) {
     const feedbackFiltered = feedbacks.filter( (f) => {
         const productName = f.fk_produto.nome.toLowerCase();
-        return productName.includes(textoDigitado.toLowerCase());
+        if(esgSelected == 'Todos')
+            return productName.includes(searchText.toLowerCase());
+
+        return productName.includes(searchText.toLowerCase()) && f.tipo_esg == esgSelected;
     });
     fillTable(feedbackFiltered);
-});
+}
+
+function detail(feedbackId){
+    const feedbackSelected = feedbacks.filter(f => f.id == feedbackId);
+    const date = document.querySelector('#date');
+    const state = document.querySelector('#state');
+    const esg = document.querySelector('#esg-detail');
+    const productName = document.querySelector('#product-name');
+    const comment = document.querySelector('#comment');
+    const grade = document.querySelector('#grade');
+
+    date.innerHTML = "Data: <br>";
+    state.innerHTML = "Estado: <br>";
+    esg.innerHTML = "Nota: <br>";
+    productName.innerHTML = "Nome produto: <br>";
+    comment.innerHTML = "Comentário: <br>";
+    grade.innerHTML = "Nota: <br>";
+
+    date.innerHTML += formataData(feedbackSelected[0].data);
+    state.innerHTML += feedbackSelected[0].estado;
+    esg.innerHTML += feedbackSelected[0].tipo_esg;
+    productName.innerHTML += feedbackSelected[0].fk_produto.nome;
+    comment.innerHTML += feedbackSelected[0].comentario;
+    grade.innerHTML += feedbackSelected[0].nota;
+}
+
+function formataData(feedbackData){
+    feedbackData = feedbackData.replace(/\u202F/g, ' ');
+    var date = new Date(feedbackData);
+    const dataFormatada = date.toLocaleDateString('pt-BR', {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+    });
+    return dataFormatada;
+}
